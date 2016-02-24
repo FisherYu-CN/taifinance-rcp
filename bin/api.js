f9ea0bc3fa8d07655419faec3b2848f5555b1321
FisherYu <alexfisher051@gmail.com>
2016-02-24 23:46:09 +0800

    format code and add comment

--- bin/api.js
+++ bin/api.js
@@ -1,11 +1,16 @@
-#!/usr/bin/env node
+/**
+ * @file    整个API服务器的入口，主要完成一些启动服务前的准备工作，比如在node端启用babel转译
+ *          以及在开发环境中启用piping进行文件监控等等。
+ */
+
+// 开发模式下启用piping监视文件变更
 if (process.env.NODE_ENV !== 'production') {
-  if (!require('piping')({
-    hook: true,
-    ignore: /(\/\.|~$|\.json$)/i
-  })) {
-    return;
-  }
+    var piping = require('piping');
+    if (!piping({ hook: true, ignore: /(\/\.|~$|\.json|\.scss$)/i })) {
+        return;
+    }
 }
-require('../server.babel'); // babel registration (runtime transpilation for node)
+
+// 启用babel转译并启动API server
+require('../server.babel');
 require('../api/api');
--- bin/server.js
+++ bin/server.js
@@ -1,28 +1,31 @@
-#!/usr/bin/env node
-require('../server.babel'); // babel registration (runtime transpilation for node)
+/**
+ * @file    整个应用服务器的入口，主要完成一些启动服务前的准备工作，比如在node端启用babel转译，
+ *          定义全局变量以及在开发环境中启用piping进行文件监控等等。
+ */
+
+require('../server.babel');
 var path = require('path');
 var rootDir = path.resolve(__dirname, '..');
-/**
- * Define isomorphic constants.
- */
-global.__CLIENT__ = false;
-global.__SERVER__ = true;
-global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
-global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
+// 定义全局常量
+global.__CLIENT__ = false;  // 是否是客户端
+global.__SERVER__ = true;   // 是否是服务端
+global.__DISABLE_SSR__ = false; // 是否禁用服务端渲染
+global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production'; // 是否是开发环境
+
+// 开发模式下启用piping监视文件变更
 if (__DEVELOPMENT__) {
-  if (!require('piping')({
-      hook: true,
-      ignore: /(\/\.|~$|\.json|\.scss$)/i
-    })) {
-    return;
-  }
+    var piping = require('piping');
+    if (!piping({ hook: true, ignore: /(\/\.|~$|\.json|\.scss$)/i })) {
+        return;
+    }
 }
 
-// https://github.com/halt-hammerzeit/webpack-isomorphic-tools
+// 初始化全局webpack-isomorphic-tools插件并调用src/server.js启动服务器
 var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
-global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack/webpack-isomorphic-tools-configuration'))
-  .development(__DEVELOPMENT__)
-  .server(rootDir, function() {
-    require('../src/server');
-  });
+var webpackIsomorphicToolsConfig = require('../webpack/webpack-isomorphic-tools-configuration');
+global.webpackIsomorphicTools = new WebpackIsomorphicTools(webpackIsomorphicToolsConfig)
+    .development(__DEVELOPMENT__)
+    .server(rootDir, function() {
+        require('../src/server');
+    });
