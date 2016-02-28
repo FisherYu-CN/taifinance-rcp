0daab2f20d413ef0c2e324aeb3e063bacd232721
FisherYu <alexfisher051@gmail.com>
2016-02-28 23:29:12 +0800

    change Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Nav from 'react-bootstrap/lib/Nav';
 
 export default class Header extends Component {
 
     render() {
         return (
-            <div className="row border-bottom"></div>
+            <div className="row border-bottom">
+                <Nav className="navbar navbar-static-top" role="navigation">
+                    <div className="navbar-header">
+                        <a className="navbar-minimalize minimalize-style-2 btn btn-primary">
+                            <i className="fa fa-bars"></i>
+                        </a>
+                    </div>
+                </Nav>
+            </div>
         );
     }
 }
