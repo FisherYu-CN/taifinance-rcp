b4c9ed1d99cebc88578bddd68cc3213162914553
FisherYu <alexfisher051@gmail.com>
2016-03-23 17:32:33 +0800

    update breadcrumb component

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -12,14 +12,26 @@ export default class Breadcrumb extends Component {
 
     static propTypes = {
         navItems: PropTypes.object,                 // 导航项信息
-        navItemsStatus: PropTypes.object           // 导航项状态
+        navItemsStatus: PropTypes.object,          // 导航项状态
+        children: PropTypes.element
     };
 
     render() {
+
+        const {children, navItems, navItemsStatus} = this.props;
+
+        // 查找激活的叶导航项
+        let activatedLeafNavItem;
+        for (let id in navItems) {
+            if (navItems.hasOwnProperty(id) && !navItems[id].hasChildren && navItemsStatus[id].active) {
+                activatedLeafNavItem = navItems[id];
+            }
+        }
+
         return (
             <div className="row wrapper border-bottom white-bg page-heading">
                 <Col lg={10}>
-                    <h2>Test</h2>
+                    <h2>{children ? children : activatedLeafNavItem.titleId}</h2>
                 </Col>
             </div>
         );
