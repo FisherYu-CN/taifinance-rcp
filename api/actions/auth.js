acb4c3a549f6c125d3e509d8756cb71079a1da08
FisherYu <alexfisher051@gmail.com>
2016-04-18 23:54:15 +0800

    remove unnecessary api

---
+++ api/actions/auth.js
@@ -0,0 +1,49 @@
+/**
+ * 加载鉴权信息
+ *
+ * @param {Object} req HTTP请求对象
+ * @return {Object} Promise对象，当resolve时，返回session中的用户对象
+ */
+export function loadAuth(req) {
+    return Promise.resolve(req.session.user || null);
+}
+
+/**
+ * 登录
+ *
+ * @param {Object} req HTTP请求对象
+ * @return {Object} Promise对象，当resolve时，返回用户对象
+ */
+export function signin(req) {
+
+    // 暂时模拟一个用户登录过程, 假定用户名/密码为rcpuser/rcpuser
+    // 当用户名/密码正确时返回用户信息，否则返回错误信息
+    const username = req.body.name;
+    const password = req.body.password;
+
+    if (username === 'rcpuser' && password === 'rcpuser') {
+        const user = {
+            name: req.body.name,
+            password: req.body.password
+        };
+        req.session.user = user;
+        return Promise.resolve(user);
+    }
+
+    return Promise.reject('用户名密码不正确');
+}
+
+/**
+ * 登出
+ *
+ * @param {Object} req HTTP请求对象
+ * @return {Object} Promise对象，当销毁session成功时resolve并返回null
+ */
+export function signout(req) {
+    return new Promise((resolve) => {
+        req.session.destroy(() => {
+            req.session = null;
+            return resolve(null);
+        });
+    });
+}
--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -1,103 +1,159 @@
-const LOAD = 'redux-example/auth/LOAD';
-const LOAD_SUCCESS = 'redux-example/auth/LOAD_SUCCESS';
-const LOAD_FAIL = 'redux-example/auth/LOAD_FAIL';
-const LOGIN = 'redux-example/auth/LOGIN';
-const LOGIN_SUCCESS = 'redux-example/auth/LOGIN_SUCCESS';
-const LOGIN_FAIL = 'redux-example/auth/LOGIN_FAIL';
-const LOGOUT = 'redux-example/auth/LOGOUT';
-const LOGOUT_SUCCESS = 'redux-example/auth/LOGOUT_SUCCESS';
-const LOGOUT_FAIL = 'redux-example/auth/LOGOUT_FAIL';
+// Action类型常量
+const LOAD = 'taifinance-rcp/auth/LOAD';
+const LOAD_SUCCESS = 'taifinance-rcp/auth/LOAD_SUCCESS';
+const LOAD_FAIL = 'taifinance-rcp/auth/LOAD_FAIL';
+const SIGNIN = 'taifinance-rcp/auth/SIGNIN';
+const SIGNIN_SUCCESS = 'taifinance-rcp/auth/SIGNIN_SUCCESS';
+const SIGNIN_FAIL = 'taifinance-rcp/auth/SIGNIN_FAIL';
+const SIGNOUT = 'taifinance-rcp/auth/SIGNOUT';
+const SIGNOUT_SUCCESS = 'taifinance-rcp/auth/SIGNOUT_SUCCESS';
+const SIGNOUT_FAIL = 'taifinance-rcp/auth/SIGNOUT_FAIL';
 
+// 初始状态
 const initialState = {
-  loaded: false
+    loaded: false
 };
 
+// 处理Action的Reducer
 export default function reducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case LOAD:
-      return {
-        ...state,
-        loading: true
-      };
-    case LOAD_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        user: action.result
-      };
-    case LOAD_FAIL:
-      return {
-        ...state,
-        loading: false,
-        loaded: false,
-        error: action.error
-      };
-    case LOGIN:
-      return {
-        ...state,
-        loggingIn: true
-      };
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        loggingIn: false,
-        user: action.result
-      };
-    case LOGIN_FAIL:
-      return {
-        ...state,
-        loggingIn: false,
-        user: null,
-        loginError: action.error
-      };
-    case LOGOUT:
-      return {
-        ...state,
-        loggingOut: true
-      };
-    case LOGOUT_SUCCESS:
-      return {
-        ...state,
-        loggingOut: false,
-        user: null
-      };
-    case LOGOUT_FAIL:
-      return {
-        ...state,
-        loggingOut: false,
-        logoutError: action.error
-      };
-    default:
-      return state;
-  }
+    switch (action.type) {
+
+        // 加载用户信息
+        case LOAD: {
+            return {
+                ...state,
+                loading: true
+            };
+        }
+
+        // 加载用户信息成功
+        case LOAD_SUCCESS: {
+            return {
+                ...state,
+                loading: false,
+                loaded: true,
+                user: action.result
+            };
+        }
+
+        // 加载用户信息失败
+        case LOAD_FAIL: {
+            return {
+                ...state,
+                loading: false,
+                loaded: false,
+                error: action.error
+            };
+        }
+
+        // 登录
+        case SIGNIN: {
+            return {
+                ...state,
+                signingIn: true
+            };
+        }
+
+        // 登录成功
+        case SIGNIN_SUCCESS:
+        {
+            return {
+                ...state,
+                signingIn: false,
+                user: action.result
+            };
+        }
+
+        // 登录失败
+        case SIGNIN_FAIL: {
+            return {
+                ...state,
+                signingIn: false,
+                user: null,
+                loginError: action.error
+            };
+        }
+
+        // 登出
+        case SIGNOUT: {
+            return {
+                ...state,
+                signingOut: true
+            };
+        }
+
+        // 登出成功
+        case SIGNOUT_SUCCESS: {
+            return {
+                ...state,
+                signingOut: false,
+                user: null
+            };
+        }
+
+        // 登出失败
+        case SIGNOUT_FAIL: {
+            return {
+                ...state,
+                signingOut: false,
+                signoutError: action.error
+            };
+        }
+
+        default:
+            return state;
+    }
 }
 
+/**
+ * 判断用户信息是否已加载
+ * @param {Object} globalState 全局状态对象
+ * @return {boolean} 用户信息是否已加载
+ */
 export function isLoaded(globalState) {
-  return globalState.auth && globalState.auth.loaded;
+    return globalState.auth && globalState.auth.loaded;
 }
 
+// Action生成函数
+/**
+ * 加载用户信息
+ *
+ * @return {Object} 加载用户信息的Action
+ */
 export function load() {
-  return {
-    types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
-    promise: (client) => client.get('/loadAuth')
-  };
+    return {
+        types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
+        promise: (client) => client.get('/loadAuth')
+    };
 }
 
-export function login(name) {
-  return {
-    types: [LOGIN, LOGIN_SUCCESS, LOGIN_FAIL],
-    promise: (client) => client.post('/login', {
-      data: {
-        name: name
-      }
-    })
-  };
+/**
+ * 登录
+ *
+ * @param {string} 用户名
+ * @param {string} 密码
+ * @return {Object} 登录Action
+ */
+export function signin(name, password) {
+    return {
+        types: [SIGNIN, SIGNIN_SUCCESS, SIGNIN_FAIL],
+        promise: (client) => client.post('/signin', {
+            data: {
+                name: name,
+                password: password
+            }
+        })
+    };
 }
 
-export function logout() {
-  return {
-    types: [LOGOUT, LOGOUT_SUCCESS, LOGOUT_FAIL],
-    promise: (client) => client.get('/logout')
-  };
+/**
+ * 注销
+ *
+ * @return {Object} 注销Action
+ */
+export function signout() {
+    return {
+        types: [SIGNOUT, SIGNOUT_SUCCESS, SIGNOUT_FAIL],
+        promise: (client) => client.get('/signout')
+    };
 }
